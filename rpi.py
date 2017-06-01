1128a465f934f620841616f046f874df52311b7f
kogita <s.kogita@gmail.com>
2017-06-01 23:25:35 +0900

    modified quitting process

--- rpi.py
+++ rpi.py
@@ -21,16 +21,22 @@ buttons = ( (LCD.SELECT, 'Select', (1,1,1)),
             (LCD.RIGHT,  'Right' , (1,0,1)) )
 
 print('Press Ctrl-C to quit.')
-while True:
-    # Loop through each button and check if it is pressed.
-    for button in buttons:
-        if lcd.is_pressed(button[0]):
-            # Button is pressed, change the message and backlight.
-            lcd.clear()
-            lcd.message(button[1])
-            lcd.set_color(button[2][0], button[2][1], button[2][2])
+try:
+    while True:
+        # Loop through each button and check if it is pressed.
+        for button in buttons:
+            if lcd.is_pressed(button[0]):
+                # Button is pressed, change the message and backlight.
+                lcd.clear()
+                lcd.message(button[1])
+                lcd.set_color(button[2][0], button[2][1], button[2][2])
 
-            camera.capture('image.jpg')
-            lcd.set_color(0.0, 0.0, 1.0)  # Blue
-            lcd.clear()
-            lcd.message('Push SELECT')
+                camera.capture('image.jpg')
+                lcd.set_color(0.0, 0.0, 1.0)  # Blue
+                lcd.clear()
+                lcd.message('Push SELECT')
+except KeyboardInterrupt:
+    lcd.set_color(0.0, 0.0, 0.0)  # Off
+    lcd.clear()
+    lcd.message('Quit')
+    print('\nQuit')
